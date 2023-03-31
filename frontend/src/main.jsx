2ff29a2931f02a1fd372a395853ecd3e31b43cb7
Evandro <evandro.souzac@gmail.com>
2023-03-31 16:36:02 -0300

    Change main to show on page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
-import { Home } from './pages/Home'
+import { NewMovie } from './pages/NewMovie'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <NewMovie />
     </ThemeProvider>
   </React.StrictMode>,
 )

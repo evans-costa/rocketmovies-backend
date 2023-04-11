d6894f4e759bd53bc2276803352270afa1e7eefd
Evandro <evandro.souzac@gmail.com>
2023-04-11 16:22:21 -0300

    Creating route to serving the file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,31 @@
-import "express-async-errors"
+import "express-async-errors";
 import express from "express";
-import AppError from './utils/AppError.js';
-import database from './database/sqlite/index.js';
-import { router } from './routes/index.js';
+import AppError from "./utils/AppError.js";
+import database from "./database/sqlite/index.js";
+import { router } from "./routes/index.js";
+import { UPLOADS_FOLDER } from "./configs/upload.js";
 
-database()
-const app = express()
+database();
+const app = express();
 
-app.use(express.json())
-app.use(router)
+app.use(express.json());
+app.use(router);
+app.use("/files", express.static(UPLOADS_FOLDER));
+
 app.use((error, req, res, next) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: "error",
-      message: error.message
-    })
+      message: error.message,
+    });
   }
   return res.status(500).json({
     status: "error",
-    message: "Internal Server Error."
-  })
-}) 
+    message: "Internal Server Error.",
+  });
+});
 
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on Port ${PORT}`)
-})
+  console.log(`Server running on Port ${PORT}`);
+});

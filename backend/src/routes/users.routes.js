4bda12e8f72ace6a2c09fb49e811596a3ef98fe7
Evandro <evandro.souzac@gmail.com>
2023-04-11 16:17:45 -0300

    Creating avatar controller and uploading

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,9 +1,20 @@
+import multer from "multer";
+import uploadConfig from "../configs/upload.js";
 import { Router } from "express";
-import { UsersController } from "../controllers/UsersController.js";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated.js";
+import { UsersController } from "../controllers/UsersController.js";
+import { UserAvatarController } from "../controllers/UserAvatarController.js";
 
+const upload = multer(uploadConfig.MULTER);
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 export const usersRouter = Router();
 
 usersRouter.post("/", usersController.create);
 usersRouter.put("/", ensureAuthenticated, usersController.update);
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);

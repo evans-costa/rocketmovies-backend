5891521abe57c6e5840b50e4e5c6b30e06f68269
Evandro <evandro.souzac@gmail.com>
2023-04-12 17:09:50 -0300

    Fix: include type button to button component

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -3,8 +3,10 @@ import { Container } from "./styles";
 export function Button({ title, icon: Icon, isRemove = false, ...rest }) {
   return (
     <Container
+      type='button'
       isRemove={isRemove}
-      {...rest}>
+      {...rest}
+    >
       {Icon && <Icon size={22} />}
       {title}
     </Container>

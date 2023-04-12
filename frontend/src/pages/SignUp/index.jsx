b1b4b761824780b128d6cf19634a98444ef5f15c
Evandro <evandro.souzac@gmail.com>
2023-04-12 15:41:18 -0300

    Creating sign up logic

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,9 +1,7 @@
-import {
-	HiOutlineMail,
-	HiOutlineLockClosed,
-	HiOutlineUser,
-	HiArrowLeft,
-} from "react-icons/hi";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { HiOutlineMail, HiOutlineLockClosed, HiOutlineUser, HiArrowLeft } from "react-icons/hi";
+import { api } from "../../services/api";
 
 import { Container, Register, Image } from "./styles";
 
@@ -12,25 +10,66 @@ import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
 
 export function SignUp() {
-	return (
-		<Container>
-			<Register>
-				<h1>RocketMovies</h1>
-				<p>Aplicação para acompanhar tudo que assistir.</p>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-				<h2>Crie sua conta</h2>
-				<Input type="text" placeholder="Nome" icon={HiOutlineUser} />
-				<Input type="email" placeholder="E-mail" icon={HiOutlineMail} />
-				<Input type="password" placeholder="Senha" icon={HiOutlineLockClosed} />
+  const navigate = useNavigate();
 
-				<Button title="Cadastrar" />
-				<ButtonText
-					to="/signin"
-					icon={HiArrowLeft}
-					title="Voltar para o login"
-				/>
-			</Register>
-			<Image />
-		</Container>
-	);
+  async function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Fill all the fields!");
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then(alert("User successfully registered!"), navigate("/"))
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("It was not possible to register...");
+        }
+      });
+  }
+
+  return (
+    <Container>
+      <Register>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Crie sua conta</h2>
+        <Input
+          type='text'
+          placeholder='Nome'
+          icon={HiOutlineUser}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          type='email'
+          placeholder='E-mail'
+          icon={HiOutlineMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type='password'
+          placeholder='Senha'
+          icon={HiOutlineLockClosed}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button
+          title='Cadastrar'
+          onClick={handleSignUp}
+        />
+        <ButtonText
+          to='/signin'
+          icon={HiArrowLeft}
+          title='Voltar para o login'
+        />
+      </Register>
+      <Image />
+    </Container>
+  );
 }
